cf6c459b9dd51c7912b257adf439c20833f4bae4
Jan Olaf Eriksen <janolaferiksen@yahoo.de>
2022-02-28 08:35:10 +0100

    Updates v2

--- db/phone.go
+++ db/phone.go
@@ -2,6 +2,32 @@ package db
 
 import "database/sql"
 
+func Open(driverName, dataSource string) (*DB, error) {
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{db}, nil
+}
+
+type DB struct {
+	db *sql.DB
+}
+
+func (db *DB) Close() error {
+	return (db.db.Close())
+}
+
+func (db *DB) Seed() error {
+	phones := []string{"1234567890", "123 456 7891", "(123) 456 7892", "(999) 000-1234", "123-456-7894", "123-456-7890", "1234567892", "(123)456-7892)"}
+	for _, phone := range phones {
+		if _, err := insertPhoneN(db.db, phone); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Migrate(driverName, dataSource string) error {
 	db, err := sql.Open(driverName, dataSource)
 	if err != nil {
@@ -48,4 +74,65 @@ func createPhoneNumbersTable(db *sql.DB) error {
 	)`
 	_, err := db.Exec(statement)
 	return err
+}
+
+func insertPhoneN(db *sql.DB, phone string) (int, error) {
+
+	// mit dem $1 umgeht ma sql injection
+	statement := `INSERT INTO phone_numbers (value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	//psql returned nicht automatisch ie id wenn geinserted wird
+	return id, nil
+}
+
+//Represents the phone_numbers
+type Phone struct {
+	Id    int
+	Value string
+}
+
+func (db *DB) AllPhones() ([]Phone, error) {
+	return allPhones(db.db)
+}
+
+func allPhones(db *sql.DB) ([]Phone, error) {
+	statement := "SELECT id, value FROM phone_numbers"
+	rows, err := db.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var ret []Phone
+	for rows.Next() {
+		var p Phone
+		if err := rows.Scan(&p.Id, &p.Value); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+
+}
+
+func findPhone(db *sql.DB, number string) (*Phone, error) {
+	statement := "SELECT * FROM phone_numbers WHERE value=$1"
+	var p Phone
+	row := db.QueryRow(statement, number)
+	err := row.Scan(&p.Id, &p.Value)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+	return &p, nil
 }
--- main.go
+++ main.go
@@ -29,45 +29,29 @@ func main() {
 	//minute 7
 	must(phonedb.Migrate("postgresql", psqlInfo))
 
-	//open db db
-	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := phonedb.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
 
-	_, err = insertPhoneN(db, "1234567890")
-	must(err)
-	_, err = insertPhoneN(db, "123 456 7891")
-	must(err)
-	_, err = insertPhoneN(db, "(123) 456 7892")
-	must(err)
-	_, err = insertPhoneN(db, "(999) 000-1234")
-	must(err)
-	_, err = insertPhoneN(db, "123-456-7894")
-	must(err)
-	_, err = insertPhoneN(db, "123-456-7890")
-	must(err)
-	_, err = insertPhoneN(db, "1234567892")
-	must(err)
-	_, err = insertPhoneN(db, "(123)456-7892)")
-	must(err)
+	if err := db.Seed(); err != nil {
+		panic(err)
+	}
 
-	//phone, err := getPhone(db, id)
-	//must(err)
-	//fmt.Printf("we found this phone number in the db %s", phone)
-	phones, err := getallPhones(db)
+	phones, err := db.AllPhones()
 	for _, p := range phones {
 		fmt.Printf("Working on... %+v\n", p)
 		number := normalize(p.Value)
 		if number != p.Value {
 			fmt.Println("Updating or removing...", number)
-			existing, err := findPhone(db, number)
-			must(err)
-			if existing != nil {
-				must(deletePhone(db, p.Id))
-			} else {
-				p.Value = number
-				err := updatePhone(db, p)
-				must(err)
-			}
+			// existing, err := findPhone(db, number)
+			// must(err)
+			// if existing != nil {
+			// 	must(deletePhone(db, p.Id))
+			// } else {
+			// 	p.Value = number
+			// 	err := updatePhone(db, p)
+			// 	must(err)
+			// }
 
 		} else {
 			fmt.Println("No changes required!")
@@ -96,72 +80,16 @@ func getPhone(db *sql.DB, id int) (string, error) {
 	return phoneNumber, nil
 }
 
-func findPhone(db *sql.DB, number string) (*phone, error) {
-	statement := "SELECT * FROM phone_numbers WHERE value=$1"
-	var p phone
-	row := db.QueryRow(statement, number)
-	err := row.Scan(&p.Id, &p.Value)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
-		} else {
-			return nil, err
-		}
-	}
-	return &p, nil
-}
-
-func updatePhone(db *sql.DB, p phone) error {
-	statement := "UPDATE phone_numbers SET value=$2 WHERE id=$1"
-	_, err := db.Exec(statement, p.Id, p.Value)
-	return err
-}
+// func updatePhone(db *sql.DB, p phone) error {
+// 	statement := "UPDATE phone_numbers SET value=$2 WHERE id=$1"
+// 	_, err := db.Exec(statement, p.Id, p.Value)
+// 	return err
+// }
 
 func deletePhone(db *sql.DB, id int) error {
 	statement := "DELETE FROM phone_numbers WHERE id=$1"
 	_, err := db.Exec(statement, id)
 	return err
-}
-
-type phone struct {
-	Id    int
-	Value string
-}
-
-func getallPhones(db *sql.DB) ([]phone, error) {
-	statement := "SELECT id, value FROM phone_numbers"
-	rows, err := db.Query(statement)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var ret []phone
-	for rows.Next() {
-		var p phone
-		if err := rows.Scan(&p.Id, &p.Value); err != nil {
-			return nil, err
-		}
-		ret = append(ret, p)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return ret, nil
-
-}
-
-func insertPhoneN(db *sql.DB, phone string) (int, error) {
-
-	// mit dem $1 umgeht ma sql injection
-	statement := `INSERT INTO phone_numbers (value) VALUES($1) RETURNING id`
-	var id int
-	err := db.QueryRow(statement, phone).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	//psql returned nicht automatisch ie id wenn geinserted wird
-	return id, nil
 }
 
 // my own working solution
